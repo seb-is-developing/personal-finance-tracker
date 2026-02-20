4f812ea180770e9321da35b81dc0d860bf0520ef
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-20 14:01:47 +0000

    feat(ui) : add initial Home Page component and styles

---
+++ client/src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <div className="title">
+        <h1>Welcome to the Personal Finance Tracker</h1>
+      </div>
+      <div className="description">
+        <p>Track your income, expenses, and savings all in one place.</p>
+      </div>
+    </>
+  );
+}
