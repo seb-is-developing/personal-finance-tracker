db5c039ed9d1e3c3d4cfe48e12c8862abd635dcb
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-20 15:41:46 +0000

    feat(ui) : add initial Home hero section structure and added font.

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,11 +1,15 @@
+import "./home.css";
+
 export default function Home() {
   return (
     <>
-      <div className="title">
-        <h1>Welcome to the Personal Finance Tracker</h1>
-      </div>
-      <div className="description">
-        <p>Track your income, expenses, and savings all in one place.</p>
+      <div className="hero-section">
+        <div className="title">
+          <h1>Welcome to the Personal Finance Tracker</h1>
+        </div>
+        <div className="description">
+          <p>Track your income, expenses, and savings all in one place.</p>
+        </div>
       </div>
     </>
   );
