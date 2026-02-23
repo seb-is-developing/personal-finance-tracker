7d12f46b3c4fd14e189939a7c655aef5613d731b
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-23 20:49:03 +0000

    feat(ui) : signup page structure added

--- client/src/pages/signUp/index.tsx
+++ client/src/pages/signUp/index.tsx
@@ -1,14 +1,18 @@
+import MainNavBar from "../../components/mainNavBar";
 export default function SignUp() {
   return (
-    <form className="register-form">
-      <h1>Sign Up</h1>
-      <label>Username:</label>
-      <input type="text" name="username" />
-      <label>Email:</label>
-      <input type="email" name="email" />
-      <label>Password:</label>
-      <input type="password" name="password" />
-      <button type="submit">Sign Up</button>
-    </form>
+    <>
+      <MainNavBar />
+      <form className="register-form">
+        <h1>Sign Up</h1>
+        <label>Username:</label>
+        <input type="text" name="username" />
+        <label>Email:</label>
+        <input type="email" name="email" />
+        <label>Password:</label>
+        <input type="password" name="password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </>
   );
 }
