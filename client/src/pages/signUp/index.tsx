a6f8c555a8b324da596df44275dd72b80737457d
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-23 21:23:11 +0000

    styling(ui) : styling added to the signup page

--- client/src/pages/signUp/index.tsx
+++ client/src/pages/signUp/index.tsx
@@ -1,18 +1,30 @@
 import MainNavBar from "../../components/mainNavBar";
+import "./signUp.css";
 export default function SignUp() {
   return (
     <>
       <MainNavBar />
-      <form className="register-form">
-        <h1>Sign Up</h1>
-        <label>Username:</label>
-        <input type="text" name="username" />
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Password:</label>
-        <input type="password" name="password" />
-        <button type="submit">Sign Up</button>
-      </form>
+      <div className="register-form-container">
+        <form className="register-form">
+          <h1 className="register-form-title">Sign Up</h1>
+          <label className="register-label">Full Name:</label>
+          <input type="text" name="fullName" className="register-input" />
+          <label className="register-label">Username:</label>
+          <input type="text" name="username" className="register-input" />
+          <label className="register-label">Email:</label>
+          <input
+            type="email"
+            name="email"
+            className="register-input"
+            placeholder="john.doe@example.com"
+          />
+          <label className="register-label">Password:</label>
+          <input type="password" name="password" className="register-input" />
+          <button className="register-button" type="submit">
+            Sign Up
+          </button>
+        </form>
+      </div>
     </>
   );
 }
