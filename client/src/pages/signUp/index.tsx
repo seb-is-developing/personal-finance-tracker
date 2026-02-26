22d33d17b5ec092b33435bb2e56d761a464069f7
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-26 18:42:49 +0000

    feat(signup): implement form inputs for user registration data

--- client/src/pages/signUp/index.tsx
+++ client/src/pages/signUp/index.tsx
@@ -1,11 +1,32 @@
 import MainNavBar from "../../components/mainNavBar";
 import "./signUp.css";
+import { useState } from "react";
+
 export default function SignUp() {
+  const [userData, setUserData] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({
+      ...prevData,    
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <MainNavBar />
       <div className="register-form-container">
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
           <h1 className="register-form-title">Sign Up</h1>
           <label className="register-label">Full Name:</label>
           <input
@@ -13,6 +34,8 @@ export default function SignUp() {
             name="fullName"
             className="register-input"
             placeholder="John Doe"
+            value={userData.fullName}
+            onChange={handleChange}
           />
           <label className="register-label">Username:</label>
           <input
@@ -20,6 +43,8 @@ export default function SignUp() {
             name="username"
             className="register-input"
             placeholder="johndoe123"
+            value={userData.username}
+            onChange={handleChange}
           />
           <label className="register-label">Email:</label>
           <input
@@ -27,6 +52,8 @@ export default function SignUp() {
             name="email"
             className="register-input"
             placeholder="john.doe@example.com"
+            value={userData.email}
+            onChange={handleChange}
           />
           <label className="register-label">Password:</label>
           <input
@@ -34,6 +61,8 @@ export default function SignUp() {
             name="password"
             className="register-input"
             placeholder="********"
+            value={userData.password}
+            onChange={handleChange}
           />
           <button className="register-button" type="submit">
             Sign Up
