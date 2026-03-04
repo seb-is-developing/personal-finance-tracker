c8b8e126ed2370ce197b7a1a428a5ffe59f69ed6
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-03-04 12:15:37 +0000

    feat(server): add getUsers route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller";
+import { getUsers, registerUser } from "../controllers/user.controller";
 
 const router = express.Router();
-
+router.get("/get-users", getUsers);
 router.post("/register", registerUser);
 
 export default router;
