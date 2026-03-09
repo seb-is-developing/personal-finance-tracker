b0f669a44cc600af8ecfe1ece1da191b16c67502
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-03-09 20:48:47 +0000

    chore(server): add environment variables for JWT

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { env } from "../config/env";
 
 export const signToken = (payload: any): string => {
-  return jwt.sign(payload, process.env.JWT_SECRET as string, {
+  return jwt.sign(payload, env.JWT_SECRET, {
     expiresIn: "7d",
   });
 };
